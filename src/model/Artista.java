209d59c78384f86405e8e7a28f0857b11b6a09b3
VoidRaul <raulcastri.rc@gmail.com>
2017-04-12 10:14:17 +0200

    Defined all Artista main parameters

--- src/model/Artista.java
+++ src/model/Artista.java
@@ -1,7 +1,40 @@
 package model;
 
-public class Artista {
-	private String nome;
-	private String cognome;
+import java.util.List;
 
+import javax.persistence.*;
+
+@Entity
+public class Artista {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	@ManyToMany
+	private List<Opera> opere;
+	
+	/*costruttore che inizializza la lista
+	public Artista(){
+		this.opere = new ArrayList<>();
+	}
+	*/
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public List<Opera> getOpere() {
+		return opere;
+	}
+	public void setOpere(List<Opera> opere) {
+		this.opere = opere;
+	}
 }
